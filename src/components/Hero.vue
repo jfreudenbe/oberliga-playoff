3bd69cb7e8a0a37ce5c0ebd42ce55dba14ed2d87
jfreudenbe <jfreudenbe@stud.hs-heilbronn.de>
2025-02-07 03:48:45 +0100

    Seperate hero from PlayoffTree

---
+++ src/components/Hero.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="p-4 relative pt-48 mb-20 md:py-30 bg-white text-balance overflow-hidden"
+  >
+    <div class="w-full flex-col flex relative">
+      <h1
+        class="md:text-8xl text-5xl font-black text-gray-900 text-center uppercase font-bebas tracking-wide"
+      >
+        Oberliga Playoff Prediction
+      </h1>
+      <h2
+        class="text-center md:text-2xl text-lg mt-4 md:mt-3 text-gray-700 tracking-tight font-roboto"
+      >
+        Click on a team’s logo to select a winner.
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+  methods: {},
+};
+</script>
