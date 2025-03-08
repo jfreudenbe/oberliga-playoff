543592df868ec047ce61b5a247e24e2e46e7b6a5
Julian Freudenberger <119427964+jfreudenbe@users.noreply.github.com>
2025-03-08 17:32:11 +0000

    change last updated date

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -14,7 +14,7 @@
         Click on a team’s logo to select a winner.
       </h2>
       <h3 class="text-center text-gray-600 text-xs md:text-sm font-bebas">
-        (last update: 22.02.2025 10:00)
+        (last update: 08.03.25 18:30)
       </h3>
     </div>
   </div>
